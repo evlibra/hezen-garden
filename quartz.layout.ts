cce77c1a9b4fe63ab1d2dd7863051e7945004c93
evlibra <elibra@ukr.net>
2025-02-22 22:53:23 -0500

    vault backup: 2025-02-22 22:53:23

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,8 +8,8 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      //GitHub: "https://github.com/jackyzha0/quartz",
-      //"Discord Community": "https://discord.gg/cRFFHYye7t",
+      HeZen portal: "https://hezen.us",
+      "Courses": "https://healthclub.hezen.us",
     },
   }),
 }
