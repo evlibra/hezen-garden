668b672057afdfd446957c154805312ed25fa31e
evlibra <elibra@ukr.net>
2025-02-22 23:01:09 -0500

    vault backup: 2025-02-22 23:01:08

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -18,6 +18,7 @@ export const sharedPageComponents: SharedLayout = {
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
     Component.Breadcrumbs(),
+	Component.Graph(),
     Component.ArticleTitle(),
     Component.ContentMeta(),
     Component.TagList(),
