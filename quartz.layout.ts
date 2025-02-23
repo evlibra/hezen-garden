b879a6f39d61edab12392ba740ded229a24133b3
evlibra <elibra@ukr.net>
2025-02-22 23:06:51 -0500

    vault backup: 2025-02-22 23:06:51

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -23,6 +23,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.ArticleTitle(),
     Component.ContentMeta(),
     Component.TagList(),
+	Component.Graph(),
   ],
   left: [
     Component.PageTitle(),
