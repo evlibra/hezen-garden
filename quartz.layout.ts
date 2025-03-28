3c38333068ba58515b07a993ebabf05a6fbeb123
Genii <evgenii.vasiuk@gmail.com>
2025-03-28 16:47:24 -0400

    revert - no graph

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -23,7 +23,6 @@ export const defaultContentPageLayout: PageLayout = {
     Component.ArticleTitle(),
     Component.ContentMeta(),
     Component.TagList(),
-	Component.Graph(),
   ],
   left: [
     Component.PageTitle(),
